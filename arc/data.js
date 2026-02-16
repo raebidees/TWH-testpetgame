f8a88d937ceffd853a4a00cdbde36ddb3710464d
raebidees <mclesliegilbert@gmail.com>
2026-02-16 10:16:43 -0500

    Create data.js

---
+++ arc/data.js
@@ -0,0 +1,3 @@
+var all_friends = [
+    {"name": "bloom????", "categories": ["howdidyougetthis"], "source": "https://github.com/raebidees/TWH-testpetgame/blob/main/images/BLOOM%3F%3F%3F.png?raw=true"}
+  ]
